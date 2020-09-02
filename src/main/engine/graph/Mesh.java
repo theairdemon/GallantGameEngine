922b9af335cea2c2535303bdc3705ce19637890a
theairdemon <33183391+theairdemon@users.noreply.github.com>
2020-09-02 13:22:01 -0400

    Completed Chapter 5
    made a multi-colored quad

--- src/main/engine/graph/Mesh.java
+++ src/main/engine/graph/Mesh.java
@@ -24,11 +24,13 @@ public class Mesh {
 
     private final int vaoId;
     private final int posVboId;
+    private final int colorVboId;
     private final int idxVboId;
     private final int vertexCount;
 
-    public Mesh(float[] positions, int[] indices) {
+    public Mesh(float[] positions, float[] colors, int[] indices) {
         FloatBuffer posBuffer = null;
+        FloatBuffer colorBuffer = null;
         IntBuffer indicesBuffer = null;
 
         try {
@@ -43,7 +45,59 @@ public class Mesh {
             posBuffer.put(positions).flip();
             glBindBuffer(GL_ARRAY_BUFFER, posVboId);
             glBufferData(GL_ARRAY_BUFFER, posBuffer, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+
+            // Color VBO
+            colorVboId = glGenBuffers();
+            colorBuffer = MemoryUtil.memAllocFloat(colors.length);
+            colorBuffer.put(colors).flip();
+            glBindBuffer(GL_ARRAY_BUFFER, colorVboId);
+            glBufferData(GL_ARRAY_BUFFER, colorBuffer, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+
+            // Index VBO
+            idxVboId = glGenBuffers();
+            indicesBuffer = MemoryUtil.memAllocInt(indices.length);
+            indicesBuffer.put(indices).flip();
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxVboId);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
+        } finally {
+            if (posBuffer != null) {
+                MemoryUtil.memFree(posBuffer);
+            }
+            if (colorBuffer != null) {
+                MemoryUtil.memFree(colorBuffer);
+            }
+            if (indicesBuffer != null) {
+                MemoryUtil.memFree(indicesBuffer);
+            }
         }
+    }
+
+    public int getVaoId() {
+        return vaoId;
+    }
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+    public void cleanup() {
+        glDisableVertexAttribArray(0);
+
+        // Delete the VBOs
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(posVboId);
+        glDeleteBuffers(colorVboId);
+        glDeleteBuffers(idxVboId);
+
+        // Delete the VAO
+        glBindVertexArray(0);
+        glDeleteVertexArrays(vaoId);
     }
 
 }
--- src/main/game/DummyGame.java
+++ src/main/game/DummyGame.java
@@ -4,6 +4,7 @@ import static org.lwjgl.glfw.GLFW.*;
 
 import main.engine.IGameLogic;
 import main.engine.Window;
+import main.engine.graph.Mesh;
 
 public class DummyGame implements IGameLogic {
 
@@ -11,6 +12,7 @@ public class DummyGame implements IGameLogic {
     private int direction = 0;
     private float color = 0.0f;
     private final Renderer renderer;
+    private Mesh mesh;
 
     public DummyGame() {
         renderer = new Renderer();
@@ -19,6 +21,22 @@ public class DummyGame implements IGameLogic {
     @Override
     public void init() throws Exception {
         renderer.init();
+        float[] positions = new float[] {
+                -0.5f,  0.5f,  0.0f,
+                -0.5f, -0.5f,  0.0f,
+                 0.5f, -0.5f,  0.0f,
+                 0.5f,  0.5f,  0.0f,
+        };
+        float[] colors = new float[] {
+                0.5f, 0.0f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f,
+        };
+        int[] indices = new int[] {
+                0, 1, 3, 3, 1, 2,
+        };
+        mesh = new Mesh(positions, colors, indices);
     }
 
     @Override
@@ -45,11 +63,12 @@ public class DummyGame implements IGameLogic {
     @Override
     public void render(Window window) {
         window.setClearColor(color, color, color, 0.0f);
-        renderer.render(window);
+        renderer.render(window, mesh);
     }
 
     @Override
     public void cleanup() {
         renderer.cleanup();
+        mesh.cleanup();
     }
 }
